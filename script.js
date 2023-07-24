7b78dff817c3812898729abc431e274624339eeb
adityag7522 <66068085+adityag7522@users.noreply.github.com>
2023-07-24 22:17:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your JS code here. If required.
+let namexx = prompt('Enter a string');
+
+let val;
+let taskobj={};
+let count=1;
+for(let i=0;i<namexx.length;i++){
+    if(taskobj.hasOwnProperty(namexx[i])){
+        taskobj[namexx[i]]++;
+    }else{
+        taskobj[namexx[i]]=count;
+    }
+}
+// console.log(taskobj);
+for(let i=0;i<namexx.length;i++){
+    if(taskobj[namexx[i]]===1){
+        val=namexx[i];        
+     break;
+    }
+}
+let bar = confirm(val);
+// console.log(namexx, bar);
